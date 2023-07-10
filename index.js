8da55e4452c5248633393ced4fd585c583445cdc
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-10 18:45:36 +0530

    create todo api endpoint success

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const {PORT} = require("./src/config/envs")
-const pool = require("./src/config/db")
+const pool = require("./src/config/db");
+const { createTodo } = require("./src/models/query");
 const app = express();
 
 app.use(cors());
@@ -10,6 +11,24 @@ app.use(express.json())
 app.get("/" , (req , res ) => {
     res.send("success")
 })
+
+//post todo
+app.post("/todo/create" , async (req , res)=> {
+    try {
+        const {description} = req.body
+        const newtodo = await createTodo(pool , description);
+        res.json(newtodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500);
+    }
+})
+
+//get todo
+app.get("/todo/:id" , (req , res)=> {
+    res.send(req.params)
+});
+
 
 
 app.listen(PORT , () => {
