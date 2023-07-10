644ad5fbd309ddbcc6a1cb27d223feffff624a20
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-10 18:43:58 +0530

    changed to seperate host and port for db

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const { PORT ,HOST } = require("./envs");
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgress",
-    password:"",
-    host: HOST,
-    port : PORT,
-    database: "pgtodo"
+  user: "postgres",
+  password: "",
+  host: DB_HOST,
+  port: DB_PORT,
+  database: "pgtodo",
 });
 
 module.exports = pool
